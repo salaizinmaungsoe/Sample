efc9e69aef999417fc1a01f8c3451bfb95ebcec5
salaizinmaungsoe <salaizms7395@gmail.com>
2020-03-15 18:07:34 +0630

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+print ("Hello World")
+
+x = 1
+y = 2
+z = x + y
+print(z)
+
+listA = [11,18,19,21,46]
+print(sum(listA))
+
+tupA = (100,200,500)
+print(sum(tupA))
+
+listA = [10,20,30,50,100]
+print(sum(listA, 20))
+
+count = 0
+while (count < 1):
+    count = count + 1
+    print("*")
